e3e539703ff15e61ec6929dcf15fe6a72dd9bcfc
TharmiganK <ktharmi176@gmail.com>
2021-06-01 12:43:45 +0530

    Added JUnit Tests for MimeUtil Class

---
+++ mime-native/src/test/java/org/ballerinalang/mime/util/MimeUtilTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.mime.util;
+
+import io.ballerina.runtime.api.values.BObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.ballerinalang.mime.util.MimeConstants.MEDIA_TYPE_FIELD;
+
+/**
+ * A unit test class for Mime module MimeUtil class functions.
+ */
+public class MimeUtilTest {
+
+    @Test
+    public void testIsValidContentTypeWithCorrectContentType() {
+        String contentType = "application/test+xml; charset=utf-8";
+        boolean returnVal = MimeUtil.isValidateContentType(contentType);
+        Assert.assertTrue(returnVal);
+    }
+
+    @Test
+    public void testIsValidContentTypeWithIncorrectContentType() {
+        String contentType = "testContentType";
+        boolean returnVal = MimeUtil.isValidateContentType(contentType);
+        Assert.assertFalse(returnVal);
+    }
+
+    @Test
+    public void testGetBaseTypeWithNullEntity() {
+        BObject entity = TestUtils.getNullBObject();
+        String returnVal = MimeUtil.getBaseType(entity);
+        Assert.assertNull(returnVal);
+    }
+
+    @Test
+    public void testGetContentTypeWithParametersWithNullEntity() {
+        BObject entity = TestUtils.getNullBObject();
+        String returnVal = MimeUtil.getContentTypeWithParameters(entity);
+        Assert.assertNull(returnVal);
+    }
+
+}
